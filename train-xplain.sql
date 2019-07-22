f362ca0d693fc321a81057f5770107346eee12da
Stefan Litsche <slitsche@ZALANDO-29089.corp.ad.zalando.net>
2019-07-22 15:50:45 +0200

    Update some comments for xplain

--- train-xplain.sql
+++ train-xplain.sql
@@ -265,3 +265,37 @@ explain select * from orders where orderdate = date_trunc('day',clock_timestamp(
 explain select * from orders where orderdate = date_trunc('day',clock_timestamp() - '1day'::interval);
 
 explain select * from orders where orderdate = 'yesterday';
+
+
+
+
+----- --------- --------- -------- Dependencies
+
+CREATE TABLE t1 (
+    a   int,
+    b   int
+);
+
+INSERT INTO t1 SELECT i/100, i/500
+                 FROM generate_series(1,1000000) s(i);
+
+ANALYZE t1;
+
+-- the number of matching rows will be drastically underestimated:
+EXPLAIN ANALYZE SELECT * FROM t1 WHERE (a = 1) AND (b = 0);
+
+CREATE STATISTICS s1 (dependencies) ON a, b FROM t1;
+
+ANALYZE t1;
+
+-- now the row count estimate is more accurate:
+EXPLAIN ANALYZE SELECT * FROM t1 WHERE (a = 1) AND (b = 0);
+
+PREPARE ts1 AS
+  SELECT * FROM t1 WHERE (a = $1) AND (b = $2);
+
+explain analyze execute ts1 (1,0);
+
+deallocate ts1;
+
+DROP STATISTICS s1;
